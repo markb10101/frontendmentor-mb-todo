18ac10b725d35b34a775f0a0da3bda977e9f1933
Mark Birchall <mark.birchall@gmail.com>
2020-12-23 18:25:28 +0000

    implemented drag and drop

--- todo.js
+++ todo.js
@@ -26,6 +26,10 @@ let todoListArr = [
     },
 ];
 
+const todo = {
+    indexOfTaskBeingDragged: 0,
+    indexOfTaskDraggedOver: 0
+};
 
 
 // render notice for how many tasks are left to do
@@ -46,7 +50,7 @@ const unfinishedTask = (index) => {
 }
 
 const removeTask = (index) => {
-    todoListArr.splice(index,1);
+    todoListArr.splice(index, 1);
     filterToDoList(activeFilter);
 }
 
@@ -55,11 +59,11 @@ const renderToDoList = (type) => {
 
     todoListArr.forEach((task, index) => {
         if (task.isActive && (type == 'All' || type == 'Active')) {
-            todoListHTML += `<li><button class="check" onclick="finishedTask(${index})"></button>
+            todoListHTML += `<li class="draggable" draggable="true"><button class="check" onclick="finishedTask(${index})"></button>
                             <span class="listText" onclick="finishedTask(${index})">${task.description}</span><div class="icon-cross" onclick="removeTask(${index})"></div></li>`;
         } else {
             if (!task.isActive && (type == 'All' || type == 'Completed')) {
-                todoListHTML += `<li class="complete"><button class="check on" onclick="unfinishedTask(${index})">
+                todoListHTML += `<li class="draggable complete" draggable="true"><button class="check on" onclick="unfinishedTask(${index})">
                              <div class="icon-check"></div></button>
                              <span class="listText" onclick="unfinishedTask(${index})">${task.description}</span><div class="icon-cross" onclick="removeTask(${index})"></div></li>`;
             }
@@ -90,6 +94,7 @@ const filterToDoList = (type) => {
             break;
     }
     renderToDoList(type);
+    handleDraggableItems();
 }
 
 const handleClickFilterAll = () => {
@@ -123,8 +128,8 @@ const handleClickClearCompleted = () => {
 
 const handleInputField = () => {
     inputNewTaskEl.addEventListener('keypress', (event) => {
-        if(event.key==='Enter'){
-            const newTask = {isActive:true, description: inputNewTaskEl.value};
+        if (event.key === 'Enter') {
+            const newTask = { isActive: true, description: inputNewTaskEl.value };
             todoListArr.unshift(newTask);
             filterToDoList(activeFilter);
             inputNewTaskEl.value = "";
@@ -134,10 +139,10 @@ const handleInputField = () => {
 
 handleClickAllTasksButton = () => {
     allTasksButtonEl.addEventListener('click', () => {
-        allTasksButtonEl.classList.toggle('on') ;
-        if(allTasksButtonEl.classList.contains('on')){
+        allTasksButtonEl.classList.toggle('on');
+        if (allTasksButtonEl.classList.contains('on')) {
             todoListArr.map(item => item.isActive = false);
-        }else{
+        } else {
             todoListArr.map(item => item.isActive = true);
         }
         filterToDoList(activeFilter);
@@ -151,9 +156,39 @@ const handleColorSchemeSwitch = () => {
     })
 }
 
+const handleDraggableItems = () => {
+    const listItemsArr = document.querySelectorAll('.draggable');
+    listItemsArr.forEach((item) => {
+        item.addEventListener('dragstart', () => {
+            item.classList.add('dragging');
+        })
+        item.addEventListener('dragover', event => {
+            event.preventDefault();
+
+            const activeItem = document.querySelector('.dragging');
+            todoListArr.forEach((task,index) => {
+                if(task.description==item.innerText){
+                    todo.indexOfTaskDraggedOver = index;
+                }
+            })
+            todoListArr.forEach((task,index) => {
+                if(task.description==activeItem.innerText){
+                    todo.indexOfTaskBeingDragged = index;
+                }
+            })
+        })
+        item.addEventListener('dragend', () => {
+            const taskBeingDragged = todoListArr[todo.indexOfTaskBeingDragged];
+            todoListArr.splice(todo.indexOfTaskBeingDragged,1);
+            todoListArr.splice(todo.indexOfTaskDraggedOver,0,taskBeingDragged);
+            filterToDoList(activeFilter);       
+        })
+    })
+}
+
 const colorSchemeEl = document.querySelector('.color-scheme');
 const pageContainerEl = document.querySelector('.pageContainer');
-// const inputContainerEl = document.querySelector('.inputContainer');
+
 const todoListEl = document.querySelector('.todo');
 const filterAllEl = document.querySelector('.all');
 const filterActiveEl = document.querySelector('.active');
@@ -164,7 +199,6 @@ const allTasksButtonEl = document.querySelector('.alltasks');
 
 let activeFilter = 'All';
 
-
 handleInputField();
 handleColorSchemeSwitch();
 handleClickFilterAll();
@@ -174,5 +208,6 @@ handleClickClearCompleted();
 handleClickAllTasksButton();
 
 filterToDoList(activeFilter);
+
 
 
